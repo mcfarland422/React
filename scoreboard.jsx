86182068de4a2bffcda470049aa0eb632211bfaf
Michael McFarland <gardengrove422@gmail.com>
2017-11-21 15:52:40 -0500

    fourth

--- scoreboard.jsx
+++ scoreboard.jsx
@@ -1,18 +1,46 @@
 // A component called Application
 // Components are Propercase (uppercase first letter)
 // All componets must return a single DOM element. THere are no exeptions to this.
-function Application(){
+function Header(props){
   return(
-    <div className="scoreboard">
-      <h1>MLS Semi-Final Scoreboard</h1>
+    <div className="header">
+      <h1>{props.title}</h1>
+    </div>
   )
 }
+function Team(props) {
+
+}
+function Application(props){
+  return(
+    <div className="scoreboard">
+      <Header title={props} />
+        <h1>MLS Semi-Final Scoreboard</h1>
+      </div>
+      <div className="teams">
+        <div className="team">
+          <div className="team-name">Columbus</div>
+
+
+        <div className="counter">
+          <button className="minus">-</button>
+          <div className="team-score">0</div>
+          <button className="plus">+</button>
+        </div>
+
+        </div>
+
+
+    </div>
+  );
+};
 
 // ReactDOM.render puts a component into the DOM.
 // It takes 2 args:
 // 1. What.
 // 2. Where.
+
 ReactDOM.render(
-  <div>Sanity, Check</div>
+  <Application title="MLS Semi-Final Scoreboard"/>,
   document.getElementById('root')
 );
