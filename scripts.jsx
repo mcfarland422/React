a4d65625c6671e650107869031521d00d5ed2384
Michael McFarland <gardengrove422@gmail.com>
2017-11-21 14:10:49 -0500

    second

---
+++ scripts.jsx
@@ -0,0 +1,11 @@
+
+
+// ReactDOM is available because we included reactDOM.js
+// the .render function needs React (react.js) and it takes 2 args:
+//   1 The component to render.
+//   2 WHere to render it.
+
+ReactDOM.render(
+  <h1>Hello, World</h1>,
+  document.getElementById('container')
+)
